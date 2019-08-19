803d7f30a7d86866a2aff3438dd99df215b264e6
Kim Nesselhauf <kim.nesselhauf@gmail.com>
2019-08-19 14:49:08 +1000

    done without optional

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  # verb    path        controller#action    prefix
+  #  get   '/ask', to: 'questions#ask', as: :ask
+  get '/ask', to: 'questions#ask', as: :ask
+  get '/answer', to: 'questions#answer', as: :answer
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
